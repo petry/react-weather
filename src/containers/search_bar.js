61d6d39180ac504da9d7380044ab76a9b5b4e64e
Marcos Petry <marcospetry@gmail.com>
2017-03-19 22:03:51 -0300

    add search event on search component

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: ''};
+
+    this.onInputchange = this.onInputchange.bind(this);
+  }
+
+  onInputchange(event) {
+    console.log(event.target.value);
+    this.setState({'term':event.target.value})
+  }
+
   render(){
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="get a five-day forecast in your favorite cities"
+          className ="form-control"
+          value={this.state.term}
+          onChange={this.onInputchange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>

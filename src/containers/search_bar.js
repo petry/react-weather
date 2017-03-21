4ef0740810b31517008790eae5b3e4ddc871bbf2
Marcos Petry <marcospetry@gmail.com>
2017-03-20 21:36:41 -0300

    calling fetchHeader throught SearchBar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
+import { bindActionCreators} from 'redux';
+import fetchWeather from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
 
     this.state = { term: ''};
 
     this.onInputchange = this.onInputchange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+
   }
 
   onInputchange(event) {
@@ -16,6 +21,8 @@ export default class SearchBar extends Component {
 
   onFormSubmit(event){
     event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({term:''});
   }
 
   render(){
@@ -33,4 +40,11 @@ export default class SearchBar extends Component {
       </form>
     );
   }
+
+
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather }, dispatch);
 }
+
+export default connect(null, mapDispatchToProps)(SearchBar);

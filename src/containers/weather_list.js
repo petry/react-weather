74aa0e5be8158c845440b2fbb8008007e893d464
Marcos Daniel Petry <petry@corp.globo.com>
2017-03-22 12:52:38 -0300

    added humidity and pressure

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,14 +6,15 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData){
     const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
     return (
       <tr key={cityData.city.name}>
         <td>{cityData.city.name}</td>
-        <td>
-        <Chart data={temps} color="orange"/>
-
-        </td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={pressures} color="green" /></td>
+        <td><Chart data={humidities} color="blue" /></td>
       </tr>
     );
   }
